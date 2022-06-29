3676fb87d6d04bb14ca53cd17b129cb6ea11e012
Serena <serenaelizabeth@hotmail.co.uk>
2022-06-29 15:13:52 +0100

    grid colours working

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 import Game from './Components/Game';
 
-const App: React.FC = () => {
+export interface WordObj {
+  wordArray:string[]
+}
 
+
+const App: React.FC = () => {
+  
   const [gameIsOver, setGameIsOver] = useState(false)
 
   function startGame() {
@@ -11,10 +16,16 @@ const App: React.FC = () => {
 
   }
 
+  const wordList = ["funky", "mouse", "plant", "crime", "night"];
+  let newWordArray:WordObj = {
+      wordArray: Array.from(wordList[Math.floor(Math.random() * wordList.length)]) //gets random word, creates array from letters
+    }
+
+
   return (
     <div className="App">
       {gameIsOver && <button onClick={startGame}>Start Game</button>}
-      {!gameIsOver && <Game />}
+      {!gameIsOver && <Game wordArray={newWordArray.wordArray} />}
 
     </div>
   );
--- src/Components/Game/index.tsx
+++ src/Components/Game/index.tsx
@@ -1,33 +1,50 @@
 import React, {useState} from 'react'
-import Keyboard from '../Keyboard'
+import Keyboard from '../Keyboard';
+import { WordObj } from "../../App"
 
-const Game: React.FC = () => {
 
-    const wordList = ["funky", "mouse", "plant", "crime", "night"]
-    let wordArray = Array.from(wordList[Math.floor(Math.random() * wordList.length)]) //gets random word, creates array from letters
+const Game: React.FC<WordObj> = ({wordArray}) => {
 
+    
     //create grid state
 
     const [grid, setGrid] = useState([[{letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}],[{letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}] ,[{letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}] ,[{letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}] ,[{letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}, {letter:"", color:"blue"}] ])
 
     function handleLetterInput(rowIndex: number, boxIndex: number,value:string) {
+        console.log(wordArray)
         const newArray = [...grid]; //create new array with value of grid
         newArray[rowIndex].splice(boxIndex, 1, {letter:value, color:"grey"}) //in the new array replace the changed value to include the letter & change color to grey
+
         if (grid[rowIndex][boxIndex].letter === wordArray[boxIndex]) {
-            newArray[rowIndex].splice(boxIndex, 1, {letter:value, color:"green"}) //if correct letter in correct place change color to green
-            console.log("letter match - correct index")
-        } else if (wordArray.filter(letter => letter === grid[rowIndex][boxIndex].letter).length > 0) { //use some instead of filter? better performance?
-            newArray[rowIndex].splice(boxIndex, 1, {letter:value, color:"yellow"}) //if correct letter in wrong place change color to yellow
-            console.log("letter match - wrong index")
+            
+            //if letter correct & in right place 
+
+            newArray[rowIndex].splice(boxIndex, 1, {letter:value, color:"green"}) 
+            console.log("CORRECT PLACE letter guessed is", grid[rowIndex][boxIndex].letter)
+            console.log("correct letter is", wordArray[boxIndex])
+
+            //check if all green <- amend this only run if on final letter?
+            if (grid[rowIndex].every((obj)=>obj.color === 'green')){
+                console.log("you won")
+
+                //TODO: change game over to true 
+            }
+
+            
+
+        } else if (wordArray.filter(letter => letter === grid[rowIndex][boxIndex].letter).length > 0) { 
+
+            //if letter correct & in wrong place
+
+            newArray[rowIndex].splice(boxIndex, 1, {letter:value, color:"yellow"}) 
+            
+        } else {
+            
+            console.log("wrong letter")
         }
         console.log(newArray)
         setGrid(newArray)
-        grid.forEach((word) => {
-            //console.log(wordArray)
-            if (word.join('') === wordArray.join('')) {
-                alert("You won")
-            }
-        })
+
     }
 
 
